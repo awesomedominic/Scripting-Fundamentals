e1e22ad3c5a5956a0e3e8fa8e8db4a5d3b68ab1a
awesomedominic <113389613+awesomedominic@users.noreply.github.com>
2023-10-06 13:48:36 -0700

    Reviewing for Quiz

---
+++ Scripting Examples/Assets/Scripts/Review for Quiz.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReviewforQuiz : MonoBehaviour
+{
+    public string MyName = "Dominic";
+    public int GradeLevel = 10;
+    public float GradePointAverage = 4.0f;
+    public bool WeCanUseNotesOnQuiz = true;
+
+    private int _favoriteNumber = 5;
+    private string _favoriteMovie = "Super Mario Bros Movie";
+    private float _priceOfGas = 5.87f;
+    private bool _reviewWasHelpful = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
